aab7a7a0d01a4b7e28cc56a8d6e6aea3afee3500
Keriel <keriel@jupiterflow.com>
2021-01-03 20:34:37 +0900

    :sparkles: Initial Commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const createError = require('http-errors');
+
+const express = require('express');
+
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.locals.basedir = path.join(__dirname);
+
+
+app.use(logger('dev'));
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit: "50mb", extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.locals.pretty = true;
+
+// MiddleWares
+
+// APIs
+
+// Views
+app.use('/', require('./routes/index'));
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
